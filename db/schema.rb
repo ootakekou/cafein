c4dd0dc944947e8fec739fe417a52c38fe43b8f4
ootakekou <kamanattou@gmail.com>
2021-08-23 18:40:25 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,58 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_061222) do
-
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "caves", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "drink_name", null: false
-    t.text "text", null: false
-    t.integer "wifi"
-    t.integer "oshare"
-    t.integer "shizuka"
-    t.integer "concent"
-    t.integer "speak"
-    t.integer "bright_room"
-    t.integer "dark_room"
-    t.integer "pet"
-    t.integer "smoke_room"
-    t.string "address", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
-    t.float "longitude"
-    t.index ["user_id"], name: "index_cafes_on_user_id"
-  end
-
-  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "cafe_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["cafe_id"], name: "index_likes_on_cafe_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2021_08_11_092748) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -81,8 +30,4 @@ ActiveRecord::Schema.define(version: 2021_08_20_061222) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "caves", "users"
-  add_foreign_key "likes", "caves"
-  add_foreign_key "likes", "users"
 end

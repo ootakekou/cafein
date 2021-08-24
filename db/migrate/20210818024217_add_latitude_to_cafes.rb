a5e18db896c6008aced4e9bd6cb2e8093537ddd0
ootakekou <kamanattou@gmail.com>
2021-08-24 12:23:56 +0900

    Update 20210818024217_add_latitude_to_cafes.rb

--- db/migrate/20210818024217_add_latitude_to_cafes.rb
+++ db/migrate/20210818024217_add_latitude_to_cafes.rb
@@ -2,6 +2,6 @@ class AddLatitudeToCafes < ActiveRecord::Migration[6.0]
   def change
 
     add_column :caves, :latitude, :float
-    add_column :cafes, :longitude, :float
+    add_column :caves, :longitude, :float
   end
 end
